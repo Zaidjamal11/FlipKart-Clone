10f3ba8505484dba6cfb8934ecc12cc2c7f207ee
unknown <zaidj777@gmail.com>
2023-06-20 01:45:53 +0530

    Banner Feature Added

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Box, styled } from '@mui/material';
 
 
 //components
-import NavBar from './NavBar';
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+
+
+const Component = styled(Box)`
+ padding: 10px 10px;
+ background: #f2f2f2;
+`
 
 const Home = () => {
   return (
-    <NavBar />
-  )
-}
+    <div>
+      <NavBar />
+      <Component>
+         <Banner />
+
+      </Component>
+      
+    </div>
+  );
+};
 
 export default Home;
